f0c7604d9283a9a7cf84e3483fe139ece63ab02f
Vlad Morozov <vlad.morozov@heygen.com>
2025-09-01 14:24:28 +0300

    remove session config

--- apps/demo/app/api/get-access-token/route.ts
+++ apps/demo/app/api/get-access-token/route.ts
@@ -1,16 +1,11 @@
-const API_KEY = "9f691626-7872-11f0-9f9a-0242c0a86102";
+const API_KEY = "";
 
-const payload = {
-  avatar_id: "9a596fce-7946-11f0-9f9a-0242c0a86102",
-  voice_id: "e706eaeb-7946-11f0-9f9a-0242c0a86102",
-  context_id: "30bfad5d-7947-11f0-9f9a-0242c0a86102",
-  language: "en",
-};
+const payload = {};
+
+const baseApiUrl = "";
 
 export async function POST() {
   try {
-    const baseApiUrl = "https://0569dd95029b.ngrok-free.app";
-
     const res = await fetch(`${baseApiUrl}/v1/sessions/token`, {
       method: "POST",
       headers: {
