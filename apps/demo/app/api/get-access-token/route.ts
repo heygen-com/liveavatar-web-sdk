2666975a20ef359e3905a3cae3386a1f25767451
Vlad Morozov <vlad.morozov@heygen.com>
2025-09-15 20:07:11 +0300

    cleanup

--- apps/demo/app/api/get-access-token/route.ts
+++ apps/demo/app/api/get-access-token/route.ts
@@ -1,11 +1,6 @@
-const API_KEY = "b918fce0-8a83-11f0-9a3d-06ce79efcd67";
+const API_KEY = "";
 
-const payload = {
-  avatar_id: "d63b120e-8a77-11f0-9a3d-06ce79efcd67",
-  context_id: "6210cccd-e84c-4df5-bbdc-a932c0c90189",
-  language: "en",
-  voice_id: "4aad65a8-8a77-11f0-9a3d-06ce79efcd67",
-};
+const payload = {};
 
 const baseApiUrl = "https://api.liveavatar.dev";
 
