660c35c7c3b0c0b9bf71562c1990cd60c91bed11
David Y <dyi@heygen.com>
2025-10-17 15:02:19 -0700

    DO NOT LAND

--- apps/demo/app/api/secrets.ts
+++ apps/demo/app/api/secrets.ts
@@ -1,2 +1,2 @@
-export const API_KEY = "YOUR_API_KEY";
-export const API_URL = "https://api.liveavatar.com";
+export const API_KEY = "5b620b3f-aeea-11f0-9a3d-06ce79efcd67";
+export const API_URL = "http://localhost:8000";
