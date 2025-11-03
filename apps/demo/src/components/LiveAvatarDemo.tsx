b6e8d92611839d4095cf1abd8a582de9a5ae4a11
David Y <dyi@heygen.com>
2025-11-03 09:06:44 -0800

    improving error messaging

--- apps/demo/src/components/LiveAvatarDemo.tsx
+++ apps/demo/src/components/LiveAvatarDemo.tsx
@@ -6,21 +6,35 @@ import { LiveAvatarSession } from "./LiveAvatarSession";
 export const LiveAvatarDemo = () => {
   const [sessionToken, setSessionToken] = useState("");
   const [mode, setMode] = useState<"FULL" | "CUSTOM">("FULL");
+  const [error, setError] = useState<string | null>(null);
 
   const handleStart = async () => {
-    const res = await fetch("/api/start-session", {
-      method: "POST",
-    });
-    const { session_token } = await res.json();
-
-    setSessionToken(session_token);
-    setMode("FULL");
+    try {
+      const res = await fetch("/api/start-session", {
+        method: "POST",
+      });
+      if (!res.ok) {
+        const error = await res.json();
+        setError(error.error);
+        return;
+      }
+      const { session_token } = await res.json();
+      setSessionToken(session_token);
+      setMode("FULL");
+    } catch (error: unknown) {
+      setError((error as Error).message);
+    }
   };
 
   const handleStartCustom = async () => {
     const res = await fetch("/api/start-custom-session", {
       method: "POST",
     });
+    if (!res.ok) {
+      const error = await res.json();
+      setError(error.error);
+      return;
+    }
     const { session_token } = await res.json();
     setSessionToken(session_token);
     setMode("CUSTOM");
@@ -35,6 +49,11 @@ export const LiveAvatarDemo = () => {
     <div className="w-full h-full flex flex-col items-center justify-center gap-4">
       {!sessionToken ? (
         <>
+          {error && (
+            <div className="text-red-500">
+              {"Error getting session token: " + error}
+            </div>
+          )}
           <button
             onClick={handleStart}
             className="w-fit bg-white text-black px-4 py-2 rounded-md"
