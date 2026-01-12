7d8eb7231d25ddfff9fd6747fe41430dceea8185
projectsaffi-coder <projectsaffi@gmail.com>
2026-01-12 14:02:54 -0500

    chore: remove next.config.js/cjs (ESM)

--- apps/demo/next.config.cjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    externalDir: true
-  },
-  transpilePackages: ['@heygen/liveavatar-web-sdk']
-};
-
-module.exports = nextConfig;
