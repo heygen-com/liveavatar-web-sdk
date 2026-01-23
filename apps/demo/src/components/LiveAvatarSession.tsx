4e5b0ef84288f7627a74762818af5d97518d4a5b
projectsaffi-coder <projectsaffi@gmail.com>
2026-01-23 14:23:31 -0500

    fix: remove @heygen/liveavatar-web-sdk import for vercel build

--- apps/demo/src/components/LiveAvatarSession.tsx
+++ apps/demo/src/components/LiveAvatarSession.tsx
@@ -8,7 +8,7 @@ import {
   useVoiceChat,
 } from "../liveavatar";
 import { useLiveAvatarContext } from "../liveavatar/context";
-import { SessionState } from "@heygen/liveavatar-web-sdk";
+import { SessionState } from "../../../packages/js-sdk/src";
 import { useAvatarActions } from "../liveavatar/useAvatarActions";
 
 const Button: React.FC<{
