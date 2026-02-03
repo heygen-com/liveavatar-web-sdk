af644e4c5bed81e43e4e76b1bc122adff12db311
David Y <dyi@heygen.com>
2026-02-03 12:18:05 -0800

    rename CUSTOM => LITE mode

--- apps/demo/app/api/secrets.ts
+++ apps/demo/app/api/secrets.ts
@@ -12,6 +12,6 @@ export const VOICE_ID = "c2527536-6d1f-4412-a643-53a3497dada9";
 export const CONTEXT_ID = "5b9dba8a-aa31-11f0-a6ee-066a7fa2e369";
 export const LANGUAGE = "en";
 
-// CUSTOM MODE Customizations
+// LITE MODE Customizations
 export const ELEVENLABS_API_KEY = "YOUR_ELEVENLABS_API_KEY";
 export const OPENAI_API_KEY = "YOUR_OPENAI_API_KEY";
